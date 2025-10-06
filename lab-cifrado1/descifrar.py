b2611136e63af2a340fdd1059c6ba4da92ddef28
Alexander Perez Sanchez <alexanderps4605@gmail.com>
2025-10-06 21:22:40 +0200

    descifrar.py sin comentarios

--- lab-cifrado1/descifrar.py
+++ lab-cifrado1/descifrar.py
@@ -1,13 +1,7 @@
 import re
-#Cargar el mensaje cifrado
 with open("NOMBRE_ARCHIVO_MENSAJE_CIFRADO","r",encoding="utf-8") as m:
     texto = m.read()
-
-#Diccionario de sustituciones que se van  deduciendo
-D={
-#'CHAR QUE SE QUIERE SUSTITUIR':'char por el que se quiere sustituir',
-#'':''
-}
+D={}
 
 def descifrar(txt, mapa):
     out=[]
